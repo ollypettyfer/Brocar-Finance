5908b92db01e422da693ab405afa50323c3a312e
Oliver Pettyfer <oliverpettyfer@Olivers-MacBook-Pro.local>
2022-02-24 17:50:47 +0000

    install bulma and start contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,98 @@ import React from "react";
 
 export const Contact = () => {
   return (
-    <div>
-      <image></image>
-      <h1>Contact</h1>
-    </div>
+    <>
+      {/* column 1 */}
+      <div class="columns">
+        <div class="column">
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input className="input" type="text" placeholder=""></input>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Conact Number</label>
+            <div className="control has-icons-left has-icons-right">
+              <input
+                className="input "
+                type="text"
+                placeholder=""
+                value=""
+              ></input>
+              <span className="icon is-small is-left">
+                <i className="fas fa-user"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-check"></i>
+              </span>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control has-icons-left has-icons-right">
+              <input
+                className="input"
+                type="email"
+                placeholder="Email input"
+                value="hello@"
+              ></input>
+              <span className="icon is-small is-left">
+                <i className="fas fa-envelope"></i>
+              </span>
+              <span className="icon is-small is-right">
+                <i className="fas fa-exclamation-triangle"></i>
+              </span>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Message</label>
+            <div className="control">
+              <textarea
+                className="textarea"
+                placeholder="What would you like to talk to us about today?"
+              ></textarea>
+            </div>
+          </div>
+
+          <div className="field is-grouped">
+            <div className="control">
+              <button className="button is-link">Submit</button>
+            </div>
+          </div>
+
+          {/* column 2 */}
+        </div>
+        <div class="column">
+          <div className="field">
+            <label className="label">GET IN TOUCH</label>
+          </div>
+
+          <div className="field">
+            <label className="label">CALL NOW</label>
+
+            <h2 className="">07496555110</h2>
+          </div>
+
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control has-icons-left has-icons-right">
+              <p>BROCARFINACE@GMAIL.COM</p>
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">OPENING HOURS</label>
+            <h2>Mon-Fri</h2>
+            <p>09:00-1700</p>
+            <h2>Sat</h2>
+            <p>09:00-1300</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,20 +1,62 @@
 import React from "react";
+import bulma from "bulma";
 import { Link } from "react-router-dom";
 import logo from "../images/logo.jpg";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar-wrapper">
-      <div className="navbar-items">
-        <img src={logo} alt="car-logo"></img>
-        <ul>
-          <Link to="/about-us">ABOUT US</Link>
-          <Link to="/finance-options">FINANCE OPTIONS</Link>
-          <Link to="car-types">CAR TYPES</Link>
-          <Link to="contact">CONTACT US</Link>
-        </ul>
-        <button className="btn-request-quote">REQUEST A QUOTE</button>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="container">
+          <div id="navMenu" className="navbar-menu">
+            <div className="navbar-start">
+              <Link to="/" className="navbar-item">
+                BROCAR FIANCE
+              </Link>
+            </div>
+
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="buttons">
+                  <Link to="finance-options" className="button is-danger">
+                    Finace Options
+                  </Link>
+                  <Link to="car-types" className="button is-dark">
+                    Car Types
+                  </Link>
+                  <Link to="contact" className="button is-link">
+                    Contact
+                  </Link>
+                  <button className="button is-warning">REQUEST A QUOTE</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+    // <nav className="navbar">
+    //   <div className="container">
+    //     <div id="navMenu" className="navbar-menu">
+    //       <div className="navbar-start">
+    //         {/* <img src={logo} alt="car-logo" className="is-small
+    //         "></img> */}
+    //       </div>
+    //       <div className="navbar-end">
+    //         <div className="navbar-item">
+    //           <Link to="/about-us" className="navbar-item">
+    //             ABOUT US
+    //           </Link>
+    //           <Link to="/finance-options">FINANCE OPTIONS</Link>
+    //           <Link to="car-types">CAR TYPES</Link>
+    //           <Link to="contact">CONTACT US</Link>
+    //           <div className="buttons">
+    //             <button className="button is-light">REQUEST A QUOTE</button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </nav>
   );
 };
