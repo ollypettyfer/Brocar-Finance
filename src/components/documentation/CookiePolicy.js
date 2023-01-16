d441d611cf9db78eb9612f9a9cd1c43a6e8e5b3b
Oliver Pettyfer <oliverpettyfer@Olivers-MacBook-Pro.local>
2023-01-16 22:57:20 +0000

    update cookies policy

--- src/components/documentation/CookiePolicy.js
+++ src/components/documentation/CookiePolicy.js
@@ -2,10 +2,101 @@ import React from "react";
 
 export const CookiePolicy = () => {
   return (
-    <div>
-      CookiePolicy<div>CookiePolicy</div>;<div>CookiePolicy</div>;
-      <div>CookiePolicy</div>;<div>CookiePolicy</div>;<div>CookiePolicy</div>;
-      <div>CookiePolicy</div>;
+    <div className="container">
+      Scope of this policy <br></br>
+      <br></br> 1. Brocar Finance(we or us or our) uses cookies when you visit
+      our website, www.brocarfinance.co.uk, (the Website) to help customise the
+      Website and improve your experience using the Website. <br></br>
+      <br></br>2. This policy applies between you, the user of this Website, and
+      us, Brocar Finance, the owner and provider of this Website.<br></br>
+      <br></br> 3. When you visit the Website, and before your Website places
+      cookies on your computer, you will be presented with a message bar
+      requesting your consent to set those cookies. By giving your consent to
+      the placing of cookies, you are enabling us to provide a better experience
+      and service. You may, if you wish, deny consent to the placing of these
+      cookies; however, certain features of the Website may not function fully
+      or as intended. <br></br>
+      <br></br>4. Thiss cookie policy should be read alongside, and in addition
+      to, our Privacy Policy, which can be found at:www.brocarfinance.co.uk.
+      What are cookies? <br></br>
+      <br></br>5. Acookie is a small text file placed on your computer by this
+      Website when you visit certain parts of the Website and/or when you use
+      certain features of the Website. <br></br>
+      <br></br>6. This Website may place and access certain cookies on your
+      computer. We use these cookies to improve your experience of using the
+      Website and to improve our range of services.<br></br>
+      <br></br>
+      7. Cookies do not usually contain any information that personally
+      identifies you, the Website user. However, personal information that we
+      store about you may be linked to the information obtained from and stored
+      in cookies. For more information on how such personal information is
+      handled and stored, refer to our Privacy Policy which is available online
+      at: www.brocarfinance.co.uk.<br></br> Types of cookies<br></br>
+      <br></br> 8. This Website uses the following cookies: Type of cookie
+      Purpose Strictly necessary cookies These are cookies that are required for
+      the operation of the Website. They include, for example, cookies that
+      enable you to log into secure areas of the Website, use a shopping cart or
+      make use of e-billing services. Analytical/performance cookies These
+      cookies allow us to recognise and count the number of visitors and to see
+      how visitors move around our Website when they are using it. This helps us
+      to improve the way our Website works, for example, by ensuring that users
+      are finding what they are looking for easily. Functionality cookies These
+      are used to recognise you when you return to our Website. This enables us
+      to personalise our content for you, greet you by name and remember your
+      preferences (for example, your choice of language or region). By using the
+      Website, you agree to our placement of functionality cookies. <br></br>
+      <br></br> 9. You can find a list of the cookies that we use in the
+      attached Cookie Schedule. <br></br>10. We have carefully chosen these
+      cookies and have taken steps to ensure that your privacy is protected and
+      respected at all times. How to control your cookies <br></br>
+      <br></br>11. You can choose to enable or disable cookies in your internet
+      browser. By default, most internet browsers accept cookies but this can be
+      changed. For further details, please see the help menu in your internet
+      browser.<br></br>
+      <br></br> 12. You can switch off cookies at any time, however, you may
+      lose information that enables you to access the Website more quickly and
+      efficiently.<br></br>
+      <br></br> 13. It is recommended that you ensure that your internet browser
+      is up-to-date and that you consult the help and guidance provided by the
+      developer of your internet browser if you are unsure about adjusting your
+      privacy settings.<br></br>
+      <br></br>
+      14. For more information generally on cookies, including how to disable
+      them, please refer to aboutcookies.org. You will also find details on how
+      to delete cookies from your computer.<br></br> Changes to this policy{" "}
+      <br></br>
+      <br></br>15. Brocar Finance reserves the right to change this cookie
+      policy as we may deem necessary from time to time or as may be required by
+      law. Any changes will be immediately posted on the Website and you are
+      deemed to have accepted the terms of the cookie policy on your first use
+      of the Website following the alterations.<br></br> Contact details{" "}
+      <br></br>
+      <br></br>16. The Website is owned by Brocar Finance incorporated in
+      England and Wales with registered number 01908 032694 whose registered
+      office is at Regus House, Fairbourne Drive, Milton Keynes, MK10 9RG,
+      England.<br></br>
+      <br></br> 17. You may contact us: a. by telephone at 01908 032694. b. by
+      email atenquiries@brocarfinance.co.uk. c. using the contact form on the
+      Website.<br></br> Attribution <br></br>
+      <br></br>18. This cookie policy was created using a document from Rocket
+      Lawyer(https://www.rocketlawyer.com/gb/en). This policy was created on 09
+      January 2023. COOKIE SCHEDULE Below is a list of the cookies that we use.
+      We have tried to ensure this is complete and up to date, but if you think
+      that we have missed a cookie or there is any discrepancy, please let us
+      know. Cookies that we use on the Website: Strictly necessary cookies We
+      use the following strictly necessary cookies: Name of cookie Purpose of
+      cookie Functionality cookies We use the following functionality cookies:
+      Name of cookie Purpose of cookie Analytical/performance cookies We use the
+      following analytical/performance cookies: Name of cookie Purpose of cookie
+      Performance/statistics cookies Analytics Cookies These cookies collect
+      information about how you use our website (e.g. which Internet browser you
+      use, how often you visit our website, which pages you open, or how long
+      you stay on our website). These cookies do not store any information that
+      enables visitors to be personally identified. The information collected
+      with the help of these cookies is aggregated, and thus anonymous. These
+      cookies allow us to analyse website use such as which pages are most
+      popular and how visitors move around the site. The information collected
+      does not directly identify any individuals.
     </div>
   );
 };
